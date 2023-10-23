781bb0c441782d290873fb7bf0ce06064a87b745
Muhammad Hilman Imaduddin <hilmanimaduddin179@gmail.com>
2023-10-23 12:42:28 +0700

    third commit

--- FRONTEND/src/lib/api.ts
+++ FRONTEND/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://noesakan-back.vercel.app/api/v1",
 });
 export default API;
 
